1b1ae3ac8a2fe455038e630e40e60571ade9851d
Mateus Silva <gmateusjose6@gmail.com>
2023-05-03 15:08:28 -0300

    finishing first version

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="trackr",
+    version="0.1.0",
+    author="byebnk",
+    url="https://github.com/byebnk/trackr",
+    packages=find_packages(exclude=["tests"]),
+)
