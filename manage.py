a51d147424e34a499ceddab0eeeb746760c0549e
Mateus Silva <gmateusjose6@gmail.com>
2023-05-03 13:04:16 -0300

    adding migrations and manage.py

---
+++ manage.py
@@ -0,0 +1,50 @@
+import sys
+
+import django
+from django.conf import settings
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.messages",
+    "pgtrigger",
+    "trackr",
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+]
+
+settings.configure(
+    INSTALLED_APPS=INSTALLED_APPS,
+    TEMPLATES=TEMPLATES,
+    MIDDLEWARE=MIDDLEWARE,
+)
+
+django.setup()
+
+if __name__ == '__main__':
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
