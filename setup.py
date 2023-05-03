5d0c3a96d070e872ad2dbf105c95b71d09d32122
Mateus Silva <gmateusjose6@gmail.com>
2023-05-03 16:18:19 -0300

    updating version on setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="trackr",
-    version="0.1.0",
+    version="0.1.1",
     author="byebnk",
     url="https://github.com/byebnk/trackr",
     packages=find_packages(exclude=["tests"]),
